f464bc7d213902354e2b4728f6c80e1727062e36
code043 <procoder043@gmail.com>
2024-12-11 22:38:48 -0300

    Insert data

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
+
+type Product struct {
+	Name      string
+	Price     float64
+	Available bool
+}
 
 func main() {
 	conn := "postgres://postgres:dev@localhost:5432/go_store?sslmode=disable"
@@ -19,6 +26,9 @@ func main() {
 		log.Fatal(err)
 	}
 	createProductTable(db)
+	product := Product{"Book", 15.55, true}
+	pk := insertProduct(db, product)
+	fmt.Printf("ID = %d\n", pk)
 }
 func createProductTable(db *sql.DB) {
 	query := `CREATE TABLE IF NOT EXISTS product(
@@ -32,4 +42,13 @@ func createProductTable(db *sql.DB) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO product (name, price, available) values ($1, $2, $3) RETURNING id`
+	var pk int
+	err := db.QueryRow(query, product.Name, product.Price, product.Available).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
 }
